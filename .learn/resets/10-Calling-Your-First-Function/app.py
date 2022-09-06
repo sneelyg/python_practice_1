e860a1145f8df510a7d81a942dde74ab1f14fc7c
sneelyg <santiagoneely@gmail.com>
2022-09-06 00:45:45 +0000

    Practicando. hasta ahora ejercicio 10

--- .learn/resets/10-Calling-Your-First-Function/app.py
+++ .learn/resets/10-Calling-Your-First-Function/app.py
@@ -4,6 +4,3 @@ def is_odd(my_number):
 
 def my_main_code():
     # your code here
-    return is_odd(45346)
-
-print(my_main_code())
--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -2,7 +2,18 @@ import random
 
 def get_randomInt():
   	# CHANGE ONLY THIS LINE BELOW
-	random_number = random.random()
-	return random_number
+	random_number = random.random()*11
+	return int(random_number)
 
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
+print(get_randomInt())
 print(get_randomInt())
